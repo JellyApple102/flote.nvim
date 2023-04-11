ca036d0b9943f15e1b5dd3d8bc82a9a83b5c6a2f
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-11 09:53:20 +0800

    fix: only show q to quit when enabled

--- lua/flote/init.lua
+++ lua/flote/init.lua
@@ -46,8 +46,11 @@ local open_float = function(file_path, file_name)
     }
 
     if M.config.window_title then
-        win_opts.title = file_name .. " - press 'q' to quit"
-        win_opts.title_pos = "left"
+        win_opts.title = file_name
+        if M.config.q_to_quit then
+            win_opts.title = win_opts.title .. " - press 'q' to quit"
+            win_opts.title_pos = "left"
+        end
     end
 
     vim.api.nvim_open_win(note_buf, true, win_opts)
